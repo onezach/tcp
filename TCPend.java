55d46f87b3426bb563b18e135b3c5af0b07ac40d
onezach <87786580+onezach@users.noreply.github.com>
2022-04-12 15:11:04 -0500

    init2

--- TCPend.java
+++ TCPend.java
@@ -1,3 +1,16 @@
 public class TCPend {
-    
+
+    public static void main(String[] args) {
+        // use arguments to pass to either a sender or a receiver
+            // args.length
+    }
+
+
+    public void Sender() {
+
+    }
+
+    public void Receiver() {
+
+    }
 }
