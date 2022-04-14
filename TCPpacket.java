f6fe1956aaf34b2a7544f08ebeeb80df5769386d
pat-loughran <71353422+pat-loughran@users.noreply.github.com>
2022-04-14 01:37:12 -0500

    added serialize()

--- TCPpacket.java
+++ TCPpacket.java
@@ -1,8 +1,8 @@
 import java.net.DatagramPacket;
+import java.nio.ByteBuffer;
 
 public class TCPpacket {
-    // actual TCP packet headers
-    private DatagramPacket UDPpacket;
+    // actual TCP packet headers, will be serialized
     private int sequenceNum;
     private int ack;
     private long timeStamp;
@@ -12,17 +12,25 @@ public class TCPpacket {
     private boolean ackFlag;
     private short zeros;
     private short checksum;
+    private byte[] payload;
 
     // constants and other attributes
     private static final int MAX_DATA_LEN = 1446;
 
     // Constructor
-    public TCPpacket(DatagramPacket UDPpacket) {
-        this.UDPpacket = UDPpacket;
+    public TCPpacket() {
+        this.zeros = 0;
+    }
+
+    public TCPpacket(byte[] payload) {
+        this.payload = payload;
         this.zeros = 0;
     }
 
     // Setters
+    public void setPayload(byte[] payload) {
+        this.payload = payload;
+    }
     public void setSequenceNum(int sequenceNum) {
         this.sequenceNum = sequenceNum;
     }
@@ -49,8 +57,8 @@ public class TCPpacket {
     }
 
     // Getters
-    public DatagramPacket getUDPpacket() {
-        return this.UDPpacket;
+    public byte[] getPayload() {
+        return this.payload;
     }
     public int getSequenceNum() {
         return this.sequenceNum;
@@ -77,13 +85,61 @@ public class TCPpacket {
         return this.checksum;
     }
 
+    // internal helper methods
+
+    private int determineLengthAndFlags() {
+        int length = this.length;
+        boolean synFlag = this.synFlag;
+        boolean finFlag = this.finFlag;
+        boolean ackFlag = this.ackFlag;
+        int serializedLength = length << 3;
+        int synMask = 1 << 2;
+        int finMask = 1 << 1;
+        int ackMask = 1;
+
+        if (synFlag) {
+            serializedLength = serializedLength | synMask;
+        }
+        if (finFlag) {
+            serializedLength = serializedLength | finMask;
+        }
+        if (ackFlag) {
+            serializedLength = serializedLength | ackMask;
+        }
+
+        return serializedLength;
+
+    }
+
     // Useful Methods
 
     public byte[] serialize() {
-        return null;
+        // find length of total TCP Packet
+        int length = 24; // length of TCP header
+        if (this.payload != null) {
+            length += this.payload.length;
+        }
+        // create byte[]
+        byte[] data = new byte[length];
+        ByteBuffer bb = ByteBuffer.wrap(data);
+
+        // add header info to byte[]
+        bb.putInt(this.sequenceNum);
+        bb.putInt(this.ack);
+        bb.putLong(this.timeStamp);
+        bb.putInt(determineLengthAndFlags()); // compute length along with flags
+        bb.putShort(this.zeros);
+        bb.putShort(this.checksum);
+
+        // add payload to byte[] if present
+        if (this.payload != null) {
+            bb.put(this.payload);
+        }
+        return data;
     }
 
-    public TCPpacket deserialize() {
+    public TCPpacket deserialize(byte[] data) {
+
         return null;
     }
 
@@ -99,9 +155,16 @@ public class TCPpacket {
         str+= "ackFlag: " + this.ackFlag + "\n";
         str+= "zeros: " + this.zeros + "\n";
         str+= "checksum: " + this.checksum + "\n";
-        str+= "datagram buffer length: " + this.UDPpacket.getLength();
-        str+= "datagram addr: " + this.UDPpacket.getAddress().toString();
-        str+= "datagram portNum: " + this.UDPpacket.getPort();
+        if (this.payload != null) {
+            str+= "payLoad length: " + this.payload.length + "\n";
+            if (this.payload.length > 20){
+                str+= "first 20 characters: " + new String(this.payload, 0, 80) + "\n";
+                str+= "last 20 characters: " + new String(this.payload, this.payload.length - 81, 80) + "\n";
+            }
+        }
+        else {
+            str+= "Payload: null\n";
+        }
         return str;
     }
 
